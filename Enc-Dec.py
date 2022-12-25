f014f632a0b48c52bc18c14406e531bf8371ea0c
Devyanshi Srivastava <devyanshi2106@gmail.com>
2022-12-25 23:02:17 +0530

    Revert "Made changes in code"
    This reverts commit 91c538d26ecf576205dcdb9741b6a5ecc1da9f16.

---
+++ Enc-Dec.py
@@ -0,0 +1,35 @@
+
+def encrypt():
+    file1 = open("data.txt", "r")
+    content = file1.read()
+
+    list1 = []
+    encrypt_list = []
+    
+    list1 = list(content)
+    for i in list1:
+         char = ord(i)
+         char+=23
+         encrypt_list.append(char)
+    print(encrypt_list)
+
+
+
+def decrypt():
+    pass
+
+
+print("""Enter 1 to encrypt
+Enter 2 to decrypt
+Enter 3 to exit""")
+ch = input("Enter your choice: ")
+
+if ch == '1':
+    encrypt()
+elif ch == '2':
+    decrypt()
+elif ch == '3':
+    exit(1)
+else:
+    print('Wrong Choice entered')
+
